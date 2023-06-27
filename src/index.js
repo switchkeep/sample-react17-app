658bd9ba590841ff8df46d6d87cb08553590d317
Thomas Uster <thomasuster@gmail.com>
2023-06-27 12:39:29 -0700

    Update migrations/test/harfoots/sample-react17-app/src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 registerServiceWorker();
