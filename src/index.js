4e491fa018660fac860137b10f699e6a376ab5f0
Smith Mathieu <smithm@lyft.com>
2023-06-28 13:53:00 -0700

    Update src/index.js: This git diff shows the changes made in a React application to replace ReactDOM.render with createRoot. The changes include:
    1. Importing createRoot from 'react-dom' instead of ReactDOM.
    
    2. Creating a constant named 'container' which refers to the root element in the DOM.
    
    3. Creating another constant named 'root' which calls the createRoot function on the 'container' constant.
    
    4. Invoking the render method on the 'root' constant and passing the App component to it.
    
    This change is part of the new ReactDOM.createRoot API which is part of the concurrent mode in React. This new API allows React to work on multiple tasks at once without blocking the main thread, making the application more responsive.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//before
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -5,4 +6,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+//after
+import React from 'react';
+import { createRoot } from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 registerServiceWorker();
