d05bcb53683c9c83d36d1571060dcccfcd19f09e
Thomas Uster <thomasuster@gmail.com>
2023-06-29 16:58:55 -0700

    Update src/App.test.js:

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import React from 'react';
+//after
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+function AppWithCallbackAfterRender() {
+  useEffect(() => { console.log('rendered'); });
+  return <App />
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const root = ReactDOM.createRoot(div);
+  root.render(<AppWithCallbackAfterRender />);
 });
