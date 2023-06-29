ac37f4987e6e371124441d950d115686c3845dc7
Thomas Uster <thomasuster@gmail.com>
2023-06-29 11:42:16 -0700

    Update src/App.test.js: The git diff shows the changes made to the file in the context of removing the callback from the render function. Here is a detailed breakdown of the changes:
    1. The 'react' import line was changed to import 'useEffect' from 'react'. This is a Hook that lets you perform side effects in function components.
    
    2. The 'react-dom' import line was changed to import 'createRoot' from 'react-dom'. This is a new API in React 18 that allows for concurrent rendering.
    
    3. The ReactDOM.render function call was replaced with a new function 'AppWithCallbackAfterRender'. This function uses the 'useEffect' hook to log 'rendered' after the App component has been rendered.
    
    4. A new constant 'root' was created using the 'createRoot' function, passing 'div' as an argument. This is used to enable concurrent rendering on the 'div' node.
    
    5. The 'root.render' function call was added to render the 'AppWithCallbackAfterRender' function. This replaces the previous ReactDOM.render call. The callback is now implemented using useEffect, which is called after the component is rendered, effectively removing the callback from the render function.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//after
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  function AppWithCallbackAfterRender() { useEffect(() => { console.log('rendered'); }); return <App />;}
+  const root = createRoot(div);
+  root.render(<AppWithCallbackAfterRender />);
 });
